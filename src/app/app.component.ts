2f09df98354b306a1a8b62413ff5aa40cf50822e
gurusaiperumalla <gurusaiperumalla.assetmonk@gmail.com>
2021-07-10 16:47:08 +0530

    added apis to service workers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-pwa';
-}
+  users: any;
+
+  constructor(private http: HttpClient  ){
+
+  }
+
+  ngOnInit(){
+    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe( (res:any) => {
+      this.users = res
+      console.log(res)
+    } , err =>{
+      console.error(err)
+    })
+  }
+} 
+
